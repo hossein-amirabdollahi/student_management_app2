4f8e567af1956ca8c4c84318dde9a0e39ca228bf
Hossein Amirabdollahi <hosseinamirabdollahi8@gmail.com>
2025-04-06 11:40:57 +0330

    Initialization project

---
+++ src/main/java/com/school/studentmanagement2/Studentmanagement2Application.java
@@ -0,0 +1,13 @@
+package com.school.studentmanagement2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Studentmanagement2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Studentmanagement2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/school/studentmanagement2/Studentmanagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.studentmanagement2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Studentmanagement2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
